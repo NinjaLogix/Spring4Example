54c657c0c9128687d72d3b7f5fbcac1e29edadf6
Unknown <branden.boyington@eagles.usm.edu>
2018-02-15 12:57:16 -0600

    Deleted Dirs
    Removed target directory because it is un-needed.

--- target/classes/sql/customerTable.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE `customer` (
-  `custid` int(11) NOT NULL,
-  `custname` varchar(45) DEFAULT NULL,
-  `city` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`custid`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1
